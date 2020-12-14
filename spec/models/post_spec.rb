71999c5990973d131d45ee1478fa886a9bd2f637
manami-m7 <kmuuum@gmail.com>
2020-12-14 10:10:19 +0000

    [finish]

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,11 +5,23 @@ RSpec.describe Post, type: :model do
         before do
             @post = Post.new
             @post.title = "Hello WebCamp"
-            @post.context = "今日も晴れです。"
+            @post.content = "今日も晴れです。"
             @post.save
         end
         it "全て入力してあるので保存される" do
             expect(@post).to be_valid
+        end
+    end
+    context "データが正しく保存されない" do
+        before do
+            @post = Post.new
+            @post.title = ""
+            @post.content = "今日も晴れです。"
+            @post.save
+        end
+        it "titleが入力されていないので保存されない" do
+            expect(@post).to be_invalid
+            expect(@post.errors[:title]).to include("can't be blank")
         end
     end
         
