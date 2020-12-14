5f8e8be5be73baf8d1e940918dac172cf0bdb17a
manami-m7 <kmuuum@gmail.com>
2020-12-14 09:34:35 +0000

    [update]

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Post, type: :model do
+    context "データが正しく保存される" do
+        before do
+            @post = Post.new
+            @post.title = "Hello WebCamp"
+            @post.context = "今日も晴れです。"
+            @post.save
+        end
+        it "全て入力してあるので保存される" do
+            expect(@post).to be_valid
+        end
+    end
+        
+end
